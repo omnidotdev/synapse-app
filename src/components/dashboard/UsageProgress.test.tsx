4505c7f443e0f41061aa3c849b22e314724c1910
Brian Cooper <brian@brian-cooper.com>
2026-04-11 13:46:18 -0500

    style: fix import ordering in UsageProgress test

--- src/components/dashboard/UsageProgress.test.tsx
+++ src/components/dashboard/UsageProgress.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "bun:test";
+
+import { render, screen } from "@testing-library/react";
 
 import UsageProgress from "./UsageProgress";
 
